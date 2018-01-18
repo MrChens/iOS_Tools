dbb40702d606b7cc87c0f451995a8ee606e1f4d0
MrChen <chenshilongfly@sina.com>
2018-01-18 14:49:38 +0800

    update resign.sh to codesign everything

--- autoResign/resign/resign.sh
+++ autoResign/resign/resign.sh
@@ -1,30 +1,33 @@
 #!/bin/sh
 
 
-if [ $# -lt 2 ]; then
+if [ $# -lt 1 ]; then
     echo ""
     echo "resign.sh Usage:"
-    echo "\t required: ./resign.sh APP_NAME.ipa APP_NAME.mobileprovision"
+    echo "\t required: ./resign.sh APP_NAME.ipa"
     exit 0
 fi
-# echo -e "panda:$PWD"
+
 source resign.config
 
-CP_IPA_PACKAGE_NAME=$1                                                         # 需要被重签的ipa名字
-TM_IPA_PACKAGE_NAME="${CP_IPA_PACKAGE_NAME%.*}_TM.ipa"                         # 重签名以后的ipa名字
-PROVISION_FILE=$2
+echo "read config.plist"
+
+TARGET_IPA_PACKAGE_NAME=$1                                                         # 需要被重签的ipa名字
+TM_IPA_PACKAGE_NAME="${TARGET_IPA_PACKAGE_NAME%.*}_TM.ipa"                         # 重签名以后的ipa名字
+PROVISION_FILE=$NEW_MOBILEPROVISION
 PAYLOAD_DIR="Payload"
-APP_DIR=
+APP_DIR=""
 CODESIGN_KEY=$CODESIGN_IDENTITIES
 ENTITLEMENTS_FILE=$ENTITLEMENTS
 OLD_MOBILEPROVISION="embedded.mobileprovision" # 配置文件
 DEVELOPER=`xcode-select -print-path`
+TARGET_APP_FRAMEWORKS_PATH=""
 
 SDK_DIR="${DEVELOPER}/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk"
 echo -e "SDK_DIR*$SDK_DIR"
-exit 1
-if [ ! -e $CP_IPA_PACKAGE_NAME ]; then
-    echo "ipa file ($CP_IPA_PACKAGE_NAME) not exist"
+
+if [ ! -e $TARGET_IPA_PACKAGE_NAME ]; then
+    echo "ipa file ($TARGET_IPA_PACKAGE_NAME) not exist"
     exit 0
 fi
 
@@ -43,11 +46,11 @@ if [ -d $PAYLOAD_DIR ]; then
 fi
 
 echo ""
-echo "1. unzip $CP_IPA_PACKAGE_NAME"
-unzip $CP_IPA_PACKAGE_NAME > /dev/null
+echo "1. unzip $TARGET_IPA_PACKAGE_NAME"
+unzip $TARGET_IPA_PACKAGE_NAME > /dev/null
 
 if [ ! -d $PAYLOAD_DIR ]; then
-    echo "unzip $CP_IPA_PACKAGE_NAME fail"
+    echo "unzip $TARGET_IPA_PACKAGE_NAME fail"
 fi
 
 for DIR in `ls $PAYLOAD_DIR/`
@@ -72,10 +75,29 @@ if [ -d $APP_DIR/_CodeSignature ]; then
     cp $PROVISION_FILE $APP_DIR/$OLD_MOBILEPROVISION
     echo ""
     echo "6. codesign....."
+#codesign frameworks
+TARGET_APP_FRAMEWORKS_PATH="$APP_DIR/Frameworks"
+if [[ -d "$TARGET_APP_FRAMEWORKS_PATH" ]]; then
+    for FRAMEWORK in "$TARGET_APP_FRAMEWORKS_PATH/"*; do
+        FILENAME=$(basename $FRAMEWORK)
+        /usr/bin/codesign -f -s "$CODESIGN_KEY" "$FRAMEWORK"
+    done
+fi
+#codesign plugins
+TARGET_APP_PLUGINS_PATH="$APP_DIR/PlugIns"
+if [[ -d "$TARGET_APP_PLUGINS_PATH" ]]; then
+    for PLUGIN in "$TARGET_APP_PLUGINS_PATH/"*; do
+        echo "PLUGIN($PLUGIN)"
+        FILENAME=$(basename $PLUGIN)
+        echo "FILENAME($FILENAME)"
+        /usr/bin/codesign -f -s "$CODESIGN_KEY" "$PLUGIN"
+    done
+fi
+
     /usr/bin/codesign -f -s "$CODESIGN_KEY" --entitlements $ENTITLEMENTS_FILE  $APP_DIR
     if [ -d $APP_DIR/_CodeSignature ]; then
         echo ""
-        echo "7. zip -r $CP_IPA_PACKAGE_NAME $APP_DIR/"
+        echo "7. zip -r $TARGET_IPA_PACKAGE_NAME $APP_DIR/"
         zip -r $TM_IPA_PACKAGE_NAME $APP_DIR/ > /dev/null
         rm -rf $APP_DIR
         echo " @@@@@@@ resign success !!!  @@@@@@@ "
